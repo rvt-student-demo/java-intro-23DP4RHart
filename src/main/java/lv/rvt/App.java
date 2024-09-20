56d918efcb0ad56590fa3d65eb0bc67658b25365
23DP4RHart <166509454+23DP4RHart@users.noreply.github.com>
2024-09-20 10:50:18 +0000

    Kalkulators EH

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,9 +1,31 @@
 package lv.rvt;
 
+import java.util.*;
+
+
 public class App 
 {
     public static void main( String[] args )
     {
-        // Make your Hello World programm!
+        double num1;
+        double num2;
+        double res;
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Ievadiet primo ciparu");
+        num1 = scan.nextDouble();
+        System.out.println("Ievadiet otro ciparu");
+        num2 = scan.nextDouble();
+
+
+       res=num1+num2;
+        System.out.println("Summation: "+res);
+        res=num1-num2;
+        System.out.println("Subtraction: "+res);
+        res=num1*num2;
+        System.out.println("Multiplication: "+res);
+        res=num1/num2;
+        System.out.println("Division: "+res);
+       
     }
 }
