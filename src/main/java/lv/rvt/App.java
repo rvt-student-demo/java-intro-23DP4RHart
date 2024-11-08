ce1283f86d7d2214549a03adf31bee65279da515
23DP4RHart <166509454+23DP4RHart@users.noreply.github.com>
2024-11-08 12:20:15 +0000

    DecreasingCounter

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -4,13 +4,32 @@ public class App {
     public class Main {
 
         public static void main(String[] args) {
-            Person ada = new Person("Ada");
-            Person antti = new Person("Antti");
-            Person martin = new Person("Martin");
+
+            
+           /// Person ada = new Person("Ada");
+           /// Person antti = new Person("Antti");
+           /// Person martin = new Person("Martin");
     
-            ada.printPerson();
-            antti.printPerson();
-            martin.printPerson();
+          ///  ada.printPerson();
+           /// antti.printPerson();
+           /// martin.printPerson();
+
+            ///Product banana = new Product("Banana");
+           /// banana.printProduct();
+           
+           DecreasingCounter counter = new DecreasingCounter(2);
+
+           counter.printValue();
+
+           counter.decrement();
+           counter.printValue();
+   
+           counter.decrement();
+           counter.printValue();
+   
+           counter.decrement();
+           counter.printValue();
+
         }
     }
 
--- src/main/java/lv/rvt/Product.java
+++ src/main/java/lv/rvt/Product.java
@@ -2,4 +2,18 @@ package lv.rvt;
 
 public class Product {
 
+    private String name;
+    private double value;
+    private double amount;
+
+    public Product(String Initialname) {
+        this.name = Initialname;
+        this.value = 1.1;
+        this.amount = 13;
+    }
+
+    public void printProduct(){
+        System.out.println(this.name + ","+ "price " + this.value + "," + this.amount );
+    }
+
 }
